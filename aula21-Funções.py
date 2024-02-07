732e549355bf5bf50e1095e44fbccfe57fcf3dfb
Josue Henrique <josue21henrique@gmail.com>
2024-02-07 14:54:20 -0300

    exercicio 102

--- aula21-Funções.py
+++ aula21-Funções.py
@@ -1,16 +1,29 @@
 # DESAFIO 101 - Funções para votação
 
-from datetime import date
+# from datetime import date
 
-def voto(idade):
-    if idade >= 16 and idade <= 18 or idade >= 70:
-        msg = 'VOTO OPCIONAL!'
-    elif idade < 16:
-        msg = 'NÃO VOTA!'
+# def voto(idade):
+#     if idade >= 16 and idade <= 18 or idade >= 70:
+#         msg = 'VOTO OPCIONAL!'
+#     elif idade < 16:
+#         msg = 'NÃO VOTA!'
+#     else:
+#         msg = 'VOTO OBRIGATORIO!'
+#     return msg
+
+# ano = int(input('Em que ano você nasceu? '))
+# idade = date.today().year - ano
+# print(f'Com {idade} anos: {voto(idade)}')
+
+# DESAFIO 102 - Função para Fatorial
+
+def fatorial(n, show=True):
+    if n == 0 or n == 1:
+        return 1
     else:
-        msg = 'VOTO OBRIGATORIO!'
-    return msg
+        if show:
+            print(f'{n} x', end=' ')
+        return n * fatorial(n-1)
+    
 
-ano = int(input('Em que ano você nasceu? '))
-idade = date.today().year - ano
-print(f'Com {idade} anos: {voto(idade)}')
+print(fatorial(5, show=False))
