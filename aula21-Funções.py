503db3cb228c2793f9b4f7d2dadf2c7475cfe18f
Josue Henrique <josue21henrique@gmail.com>
2024-02-09 15:38:00 -0300

    terminado atividade 103

--- aula21-Funções.py
+++ aula21-Funções.py
@@ -17,16 +17,34 @@
 
 # DESAFIO 102 - Função para Fatorial
 
-def fatorial(n, show=False):
-    f = 1
-    for c in range(n, 0, -1):
-        if show:
-            print(c, end='')
-            if c > 1:
-                print(' x ', end='')
-            else:
-                print(f' = ', end='')
-        f *= c
-    return f
+# def fatorial(n, show=False):
+#     f = 1
+#     for c in range(n, 0, -1):
+#         if show:
+#             print(c, end='')
+#             if c > 1:
+#                 print(' x ', end='')
+#             else:
+#                 print(f' = ', end='')
+#         f *= c
+#     return f
 
-print(fatorial(5, True))               
+# print(fatorial(5, True))        
+
+# DESAFIO 103 - Ficha do Jogador
+
+def ficha(nome='<desconhecido>', gols=0):
+    print(f'O jogador {nome} fez {gols} gol(s) no campeonato')
+
+jogador = str(input('Nome do Jogador: '))
+gol = str(input('Número de Gols: '))
+
+if gol.isnumeric():
+    gol = int(gol)
+else:
+    gol = 0
+
+if jogador.strip() == '':
+    ficha(gols=gol)
+else:
+    ficha(jogador, gol)
