98bd54af2e9e88a31603397ab45c2ab042ef4329
Josue Henrique <josue21henrique@gmail.com>
2024-02-14 17:16:34 -0300

    desafio 106 concluido

--- aula21-Funções.py
+++ aula21-Funções.py
@@ -71,38 +71,76 @@
 
 # DESAFIO 105 - Analisando e gerando Dicionários
 
-def notas(*n, sit=False):
-    nota = list(n)
-    resposta = dict()
+# def notas(*n, sit=False):
+#     nota = list(n)
+#     resposta = dict()
     
-    #Pegando qtd de nota
-    resposta['total'] = len(nota)
+#     #Pegando qtd de nota
+#     resposta['total'] = len(nota)
     
-    #Pegando maior nota
-    maior = 0
-    for i in nota:
-        if i > maior:
-            maior = i
-    resposta['maior'] = maior
+#     #Pegando maior nota
+#     maior = 0
+#     for i in nota:
+#         if i > maior:
+#             maior = i
+#     resposta['maior'] = maior
     
-    #Pegando menor nota
-    menor = float('inf')
-    for i in nota:
-        if i < menor:
-            menor = i
-    resposta['menor'] = menor
+#     #Pegando menor nota
+#     menor = float('inf')
+#     for i in nota:
+#         if i < menor:
+#             menor = i
+#     resposta['menor'] = menor
     
-    #Pegando a media das notas
-    resposta['media'] = sum(nota) / len(nota)
+#     #Pegando a media das notas
+#     resposta['media'] = sum(nota) / len(nota)
     
-    #Pegando a situação
-    if sit:
-        if resposta['media'] > 6:
-            resposta['situação'] = 'BOA!'
-        else:
-            resposta['situação'] = 'RUIM!'
+#     #Pegando a situação
+#     if sit:
+#         if resposta['media'] > 6:
+#             resposta['situação'] = 'BOA!'
+#         else:
+#             resposta['situação'] = 'RUIM!'
     
-    return resposta
+#     return resposta
 
-resposta = notas(5.5, 2.5, 1.5, 9, sit=True)
-print(resposta)
+# resposta = notas(5.5, 2.5, 1.5, 9, sit=True)
+# print(resposta)
+
+# DESAFIO 106 - Interactive helping system in Python
+
+from time import sleep
+
+c = ('\033[m', # sem cor
+     '\033[0;30;41m', # vermelho
+     '\033[0;30;42m', # verde
+     '\033[0;30;43m', # amarelo
+     '\033[0;30;44m', # azul
+     '\033[0;30;45m', # roxo
+     '\033[7;30m' # branco
+     );
+
+def ajuda(comando):
+    titulo(f'Acessando o manual do comando \'{comando}\'', 4)
+    sleep(3)
+    print(c[6], end='')
+    help(comando)
+    print(c[0], end='')
+    
+def titulo(msg, cor=0):
+    tam = len(msg) + 4
+    print(c[cor], end='')
+    print('~'*tam)
+    print(f'{msg}')
+    print('~'*tam)
+    print(c[0], end='')
+
+while True:
+    titulo('SISTEMA DE AJUDA PyHELP', 2)
+    pesquisa = str(input('Função ou Biblioteca > '))
+    
+    if pesquisa.upper() != 'FIM':    
+        ajuda(pesquisa)
+    else:
+        titulo('ATÉ LOGO!', 1)
+        break
