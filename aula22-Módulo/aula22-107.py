ce9cfb9a18257bf8611b769d385f2fde0ee09e2f
Josue Henrique <josue21henrique@gmail.com>
2024-02-16 14:49:51 -0300

    desafio 111 completo

--- aula22-Módulo/aula22-107.py
+++ aula22-Módulo/aula22-107.py
@@ -1,5 +1,5 @@
-from moeda import moedas
-import moeda
+from utilidadesCeV.moeda import moedas
+from utilidadesCeV import moeda
 
 numero = int(input('Digite o preço R$ '))
 moeda.resumo(numero, 20, 12)
