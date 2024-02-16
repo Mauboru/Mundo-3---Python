0fa5a084d919e94c1209e77e17e5d069818d06bd
Josue Henrique <josue21henrique@gmail.com>
2024-02-16 14:44:14 -0300

    desafio 110 completo

--- aula22-Módulo/aula22-107.py
+++ aula22-Módulo/aula22-107.py
@@ -2,8 +2,4 @@ from moeda import moedas
 import moeda
 
 numero = int(input('Digite o preço R$ '))
-
-print(f'O aumento de {moedas(numero)} é {moeda.aumentar(numero, True)}')
-print(f'A diminuição de {moedas(numero)} é {moeda.diminuir(numero)}')
-print(f'A metade de {moedas(numero)} é {moeda.metade(numero, True)}')
-print(f'O dobro de {moedas(numero)} é {moeda.dobro(numero)}')
+moeda.resumo(numero, 20, 12)
--- aula22-Módulo/moeda.py
+++ aula22-Módulo/moeda.py
@@ -1,12 +1,12 @@
 # Aumentar a porcentagem em 10%
-def aumentar(n, format=False):
-    porcentagem = (n * 10) / 100
+def aumentar(n, aumento=0, format=False):
+    porcentagem = (n * aumento) / 100
     aumento = porcentagem + n
     return aumento if format is False else moedas(aumento)
 
 # Diminuir a porcentagem em 10%
-def diminuir(n, format=False):
-    porcentagem = (n * 10) / 100
+def diminuir(n, diminuir=0, format=False):
+    porcentagem = (n * diminuir) / 100
     diminuir = n - porcentagem
     return diminuir if format is False else moedas(diminuir)
 
@@ -23,3 +23,16 @@ def metade(n, format=False):
 # Formata os valores
 def moedas(preco=0, moeda='R$'):
     return f'{moeda}{preco:.2f}'.replace('.',',')
+
+# Faz um resumo com todas as infos
+def resumo(numero, a, d):
+    msg = '--------------------------------\n'
+    msg += '        RESUMO DO VALOR        \n'
+    msg += '--------------------------------\n'
+    msg += f'Preço analisado:        {moedas(numero)}\n'
+    msg += f'Dobro do preço:        {dobro(numero, True)}\n'
+    msg += f'Metade do preço:       {metade(numero, True)}\n'
+    msg += f'{a}% de aumento:        {aumentar(numero, a, True)}\n'
+    msg += f'{d}% de redução:        {diminuir(numero, d, True)}\n'
+    msg += '--------------------------------'
+    return print(msg)
